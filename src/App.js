65b8a8201e92b0dd144ee2be3e5b71de77902ee3
Sophia-Akoth <sophiadorcas@gmail.com>
2020-09-27 21:45:36 +0300

    beginning of my spreadsheet project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Table from './components/Table'
+const App = () =>
+  (<div style={{ width: 'max-content' }}>
+    <Table x={4} y={4} />
+  </div>)
+
+export default App;
